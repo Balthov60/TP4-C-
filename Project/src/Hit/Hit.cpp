06a7595e72997212dfce4b17b3021b416a01d1db
Valentin <val.gille@sfr.fr>
2018-12-12 21:45:45 +0100

    [WIP] Finding a fix for EOF issues on anonyme.log

--- Project/src/Hit/Hit.cpp
+++ Project/src/Hit/Hit.cpp
@@ -40,7 +40,12 @@ istream & operator>>(istream & is, Hit & hit)
     hit.statusCode = (unsigned int) stoi(temp, nullptr, 10);
 
     getline(is, temp, BASIC_SEPARATOR);
-    hit.dataQty = (unsigned int) stoi(temp, nullptr, 10);
+    if (temp.c_str()[0] == '-')         //sometimes, field data quantity has '-' char
+    {                                   //thus it's necessary to test it manually to avoid stoi errors
+        hit.dataQty = 0;
+    } else {
+        hit.dataQty = (unsigned int) stoi(temp, nullptr, 10);
+    }
 
     is.seekg(1, ios_base::cur);
     getline(is, hit.referer, LONG_STRING_SEPARATOR);
@@ -48,7 +53,12 @@ istream & operator>>(istream & is, Hit & hit)
     is.seekg(2, ios_base::cur);
     getline(is, hit.browserInfo, LONG_STRING_SEPARATOR);
 
-    getline(is, temp);  // Remove end of file indicator
+    //getline(is, temp);  // Remove end of file indicator (use of this line generates an error at the end of anonyme.log)
+    //TODO : find a better fix to EOF problem. 
+    is.seekg(1, ios_base::cur);
+    if (is.get() != -1)
+        is.seekg(-1,ios_base::cur);
+
 
     return is;
 }
